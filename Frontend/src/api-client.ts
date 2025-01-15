6934d2ce5e85adf34129c119213f6f1f161f47f4
shivam kevat <webhunter81@gmail.com>
2025-01-15 21:07:33 +0530

    dist change

--- Frontend/src/api-client.ts
+++ Frontend/src/api-client.ts
@@ -7,11 +7,12 @@ import {
   UserType,
 } from "../../backend/src/shared/types";
 import { BookingFormData } from "./forms/BookingForm/BookingForm";
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
+const API_BASE_URL = "https://bds-render.onrender.com" ;
 
 export const fetchCurrentUser = async (): Promise<UserType> => {
   const response = await fetch(`${API_BASE_URL}/api/users/me`, {
     credentials: "include",
+    
   });
   if (!response.ok) {
     throw new Error("Error fetching user");
@@ -40,6 +41,7 @@ export const signIn = async (formData: SignInFormData) => {
   const response = await fetch(`${API_BASE_URL}/api/auth/login`, {
     method: "POST",
     credentials: "include",
+    
     headers: {
       "Content-Type": "application/json",
     },
@@ -55,6 +57,7 @@ export const signIn = async (formData: SignInFormData) => {
 
 export const validateToken = async () => {
   const response = await fetch(`${API_BASE_URL}/api/auth/validate-token`, {
+    method: "POST",
     credentials: "include",
   });
 
